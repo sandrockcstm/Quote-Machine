bd9bd4f20f7973d0d8d2982b1f9517acbb9d4740
Sandrockcstm <sandrockcstm@yahoo.com>
2019-03-21 15:59:31 -0500

    Removed FCC test suite

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "./App.css";
+import * as serviceWorker from "./serviceWorker";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import $ from "../node_modules/jquery";
+import Popper from "../node_modules/popper.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
