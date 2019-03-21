5fefb009ebde201e72b56faf4a0e83dbfec60dfd
sandrockcstm <sandrockcstm@yahoo.com>
2019-03-21 16:18:22 -0500

    Add files via upload

---
+++ precache-manifest.b8c65582482b13917c1dcf3ad740c754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adcb725b7984547e5c03",
+    "url": "/Quote-Machine/static/js/runtime~main.6672dbc0.js"
+  },
+  {
+    "revision": "dfe45de2c353ac66e98a",
+    "url": "/Quote-Machine/static/js/main.c8da9145.chunk.js"
+  },
+  {
+    "revision": "f15ca27610c418f93346",
+    "url": "/Quote-Machine/static/js/2.8d243238.chunk.js"
+  },
+  {
+    "revision": "dfe45de2c353ac66e98a",
+    "url": "/Quote-Machine/static/css/main.0a8af6ff.chunk.css"
+  },
+  {
+    "revision": "f15ca27610c418f93346",
+    "url": "/Quote-Machine/static/css/2.13c31332.chunk.css"
+  },
+  {
+    "revision": "0c346c26cf466b716dcb4cc0408d2739",
+    "url": "/Quote-Machine/index.html"
+  }
+];
